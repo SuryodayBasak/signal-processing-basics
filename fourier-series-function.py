389c766b0f140b6f3610a49840cc770b5ff00360
SuryodayBasak <suryodaybasak@gmail.com>
2020-09-07 17:31:59 -0400

    Initial commit.

---
+++ fourier-series-function.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sympy import *
+from sympy.plotting import plot
+
+# Find a0
+def find
+# Find an
+
+# Find bn
+
+
+# Generate function
+x = symbols('x')
+y = sin(x)
+for a in range(3, 500, 2):
+    y += sin(a * x) / a
+
+
