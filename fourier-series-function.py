f3ac44b4ccf9eb316176ce4471889b78703be004
SuryodayBasak <suryodaybasak@gmail.com>
2020-09-07 21:21:09 -0400

    Finished simple Fourier series and added audio samples.

--- fourier-series-function.py
+++ fourier-series-function.py
@@ -2,17 +2,29 @@ import numpy as np
 from sympy import *
 from sympy.plotting import plot
 
-# Find a0
-def find
-# Find an
-
-# Find bn
-
-
-# Generate function
+# Generate function.
 x = symbols('x')
 y = sin(x)
-for a in range(3, 500, 2):
+for a in range(3, 10, 2):
     y += sin(a * x) / a
 
+# Plot function.
+p1 = plot(y, title="Plot of original function.")
 
+# `n` in an and bn.
+N = 10
+
+# Find a0.
+y_ = (1/(2*pi)) * integrate(y, (x, -pi, pi))
+print(y_)
+
+# Find an.
+for n in range(1, N):
+    y_ += (1/pi) * integrate(y * cos(n*x), (x, -pi, pi)) * cos(n*x)
+
+# Find bn.
+for n in range(1, N):
+    y_ += (1/pi) * integrate(y * sin(n*x), (x, -pi, pi)) * sin(n*x)
+
+print(y_)
+p2 = plot(y_, title="Plot of estimated function.")
